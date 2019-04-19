cc71dab49b9d26b14fc6925e221dee68db6a99d1
Pattachou <chou.95230@live.fr>
2019-04-19 17:22:02 +0200

    Ajout route pour Tips

--- src/main/scala/utils/WebServer.scala
+++ src/main/scala/utils/WebServer.scala
@@ -109,11 +109,22 @@ object WebServer {
           get{
             complete("La somme des don est de " + sumDonation(DONATOR_MAP).toString)
           }
+        }~
+        path("sum_don_user" ) {
+          get{
+            complete("La somme des don par user :" + sumDonationperUser(DONATOR_MAP).toString)
+          }
+        }~
+        path("sum_dor_1_user" ) {
+          get {
+            parameter('id.as[Int]) { id => complete(sumDonationForUser(DONATOR_MAP, id)) }
+
+          }
         }
 
 
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+          val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
